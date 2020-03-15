c9973ceaa1dcaf653698fc8029d7abee2d05d3d3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-03-15 12:20:52 +0100

    Add glutin and open a window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use webrender::Renderer;
 use gleam::gl;
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
 
 fn main() {
-    
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new().with_title("A fantastic window!");
+
+    let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
+
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
+    println!(
+        "Pixel format of the window's GL context: {:?}",
+        windowed_context.get_pixel_format()
+    );
+
+    el.run(|event, target, control_flow| {
+        println!("{:?}", event);
+    });
 }

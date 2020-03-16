4abaa1483e51af80cd5b6d9dc1b0ed4db395e09c
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-03-16 20:11:19 +0100

    Copy from examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use webrender::{Renderer, RendererOptions};
-use webrender::api::{ColorF, RenderNotifier, DocumentId, DisplayListBuilder, Transaction, Epoch, PipelineId, CommonItemProperties, SpaceAndClipInfo};
-use webrender::api::units::{LayoutSize, DeviceIntSize, LayoutRect, LayoutPoint};
+use webrender::api::{ColorF, RenderNotifier, RenderApi, DocumentId, DisplayListBuilder, Transaction, Epoch, PipelineId, CommonItemProperties, SpaceAndClipInfo, PrimitiveFlags, ImageDescriptor, ImageData, ImageFormat, ImageDescriptorFlags, ComplexClipRegion, BorderRadius, ClipMode, BorderStyle, BorderDetails, ImageMask, BorderSide, NormalBorder};
+use webrender::api::units::{LayoutSize, DeviceIntSize, LayoutRect, LayoutPoint, LayoutSideOffsets};
 use gleam::gl as opengl;
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop, EventLoopProxy};
@@ -38,13 +38,82 @@ impl<T: 'static + Send> RenderNotifier for Notifier<T> {
     }
 }
 
-fn render_wr(builder: &mut DisplayListBuilder) {
-    let rect_size = LayoutRect::new(
-        LayoutPoint::new(100.0, 100.0),
-        LayoutSize::new(100.0, 200.0)
+fn render_wr(api: &RenderApi, pipeline_id: PipelineId, txn: &mut Transaction, builder: &mut DisplayListBuilder) {
+    let content_bounds = LayoutRect::new(LayoutPoint::zero(), builder.content_size());
+    let root_space_and_clip = SpaceAndClipInfo::root_scroll(pipeline_id);
+    let spatial_id = root_space_and_clip.spatial_id;
+
+    builder.push_simple_stacking_context(
+        content_bounds.origin,
+        spatial_id,
+        PrimitiveFlags::IS_BACKFACE_VISIBLE,
     );
-    let rect_props = CommonItemProperties::new(rect_size, SpaceAndClipInfo::default());
-    builder.push_rect(&rect_props, ColorF::new(0.0, 1.0, 1.0, 1.0))
+
+    let image_mask_key = api.generate_image_key();
+    txn.add_image(
+        image_mask_key,
+        ImageDescriptor::new(2, 2, ImageFormat::R8, ImageDescriptorFlags::IS_OPAQUE),
+        ImageData::new(vec![0, 80, 180, 255]),
+        None,
+    );
+    let mask = ImageMask {
+        image: image_mask_key,
+        rect: LayoutRect::new(LayoutPoint::new(75.0, 75.0), LayoutSize::new(100.0, 100.0)),
+        repeat: false,
+    };
+    let complex = ComplexClipRegion::new(
+        LayoutRect::new(LayoutPoint::new(50.0, 50.0), LayoutSize::new(100.0, 100.0)),
+        BorderRadius::uniform(20.0),
+        ClipMode::Clip
+    );
+    let clip_id = builder.define_clip(
+        &root_space_and_clip,
+        content_bounds,
+        vec![complex],
+        Some(mask)
+    );
+
+    builder.push_rect(
+        &CommonItemProperties::new(
+            LayoutRect::new(LayoutPoint::new(100.0, 100.0), LayoutSize::new(100.0, 100.0)),
+            SpaceAndClipInfo { spatial_id, clip_id },
+        ),
+        ColorF::new(0.0, 1.0, 0.0, 1.0),
+    );
+
+    builder.push_rect(
+        &CommonItemProperties::new(
+            LayoutRect::new(LayoutPoint::new(250.0, 100.0), LayoutSize::new(100.0, 100.0)),
+            SpaceAndClipInfo { spatial_id, clip_id },
+        ),
+        ColorF::new(0.0, 1.0, 0.0, 1.0),
+    );
+    let border_side = BorderSide {
+        color: ColorF::new(0.0, 0.0, 1.0, 1.0),
+        style: BorderStyle::Groove,
+    };
+    let border_widths = LayoutSideOffsets::new_all_same(10.0);
+    let border_details = BorderDetails::Normal(NormalBorder {
+        top: border_side,
+        right: border_side,
+        bottom: border_side,
+        left: border_side,
+        radius: BorderRadius::uniform(20.0),
+        do_aa: true,
+    });
+
+    let bounds = LayoutRect::new(LayoutPoint::new(100.0, 100.0), LayoutSize::new(100.0, 100.0));
+    builder.push_border(
+        &CommonItemProperties::new(
+            bounds,
+            SpaceAndClipInfo { spatial_id, clip_id },
+        ),
+        bounds,
+        border_widths,
+        border_details,
+    );
+
+    builder.pop_stacking_context();
 }
 
 fn main() {
@@ -61,27 +130,44 @@ fn main() {
     let windowed_context = unsafe { windowed_context.make_current().unwrap() };
 
     let gl = unsafe {
-        opengl::GlFns::load_with(|ptr| windowed_context.get_proc_address(ptr))
+        opengl::GlFns::load_with(
+            |symbol| windowed_context.get_proc_address(symbol) as *const _
+        )
     };
 
     let notifier = Notifier::new(&el);
-    let options = RendererOptions::default();
+    let options = RendererOptions {
+        clear_color: Some(ColorF::new(0.3, 0.0, 0.0, 1.0)),
+        ..RendererOptions::default()
+    };
     let size = DeviceIntSize::new(800, 600);
-    let layout_size = LayoutSize::new(800.0, 600.0);
     let (mut renderer, sender) = Renderer::new(gl.clone(), Box::new(notifier), options, None, size).unwrap();
 
     let api = sender.create_api();
     let doc_id = api.add_document(size, 0);
+
     let epoch = Epoch(0);
     let pipeline_id = PipelineId(0, 0);
+    let layout_size = size.to_f32() / webrender::euclid::Scale::new(1.0);
     let mut builder = DisplayListBuilder::new(pipeline_id, layout_size);
     let mut txn = Transaction::new();
-    render_wr(&mut builder);
-    txn.set_display_list(epoch, None, layout_size, builder.finalize(), true);
+
+    render_wr(&api, pipeline_id, &mut txn, &mut builder);
+
+    txn.set_display_list(
+        epoch,
+        Some(ColorF::new(0.3, 0.0, 0.0, 1.0)),
+        layout_size,
+        builder.finalize(),
+        true,
+    );
+    txn.set_root_pipeline(pipeline_id);
     txn.generate_frame();
     api.send_transaction(doc_id, txn);
 
     el.run(move |event, _target, control_flow| {
+        let mut txn = Transaction::new();
+
         match event {
             Event::WindowEvent { window_id, event } => {
                 match event {
@@ -96,11 +182,16 @@ fn main() {
             },
             Event::RedrawRequested(_) => {
                 println!("Redraw");
-                gl.clear_color(0.5, 0.0, 0.2, 1.0);
-                gl.clear(opengl::COLOR_BUFFER_BIT | opengl::DEPTH_BUFFER_BIT | opengl::STENCIL_BUFFER_BIT);
+                if txn.is_empty() {
+                    txn.generate_frame()
+                }
+
+                api.send_transaction(doc_id, txn);
+
                 renderer.update();
                 renderer.render(size).unwrap();
-                windowed_context.swap_buffers().unwrap();
+                let _ = renderer.flush_pipeline_info();
+                windowed_context.swap_buffers().ok();
             },
             Event::LoopDestroyed => (),
             _ => ()

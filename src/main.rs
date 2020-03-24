359d72f9de92884f0187249fae255c96a69c75b7
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-03-24 16:03:26 +0100

    Display static view into scrollview

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,7 @@ fn render_wr(api: &RenderApi, pipeline_id: PipelineId, txn: &mut Transaction, bu
         root_space_and_clip.spatial_id,
         PrimitiveFlags::IS_BACKFACE_VISIBLE,
     );
-    
+
     // let's make a scrollbox
     let scrollbox = (0, 0).to(300, 400);
 
@@ -116,70 +116,6 @@ fn render_wr(api: &RenderApi, pipeline_id: PipelineId, txn: &mut Transaction, bu
     );
     info.hit_info = Some((0, 3));
     builder.push_rect(&info, ColorF::new(0.0, 1.0, 0.0, 1.0));
-
-    // Below the above rectangles, set up a nested scrollbox. It's still in
-    // the same stacking context, so note that the rects passed in need to
-    // be relative to the stacking context.
-    let space_and_clip2 = builder.define_scroll_frame(
-        &space_and_clip1,
-        None,
-        (0, 100).to(300, 1000),
-        (0, 100).to(200, 300),
-        vec![],
-        None,
-        ScrollSensitivity::ScriptAndInputEvents,
-        LayoutVector2D::zero(),
-    );
-
-    // give it a giant gray background just to distinguish it and to easily
-    // visually identify the nested scrollbox
-    let mut info = CommonItemProperties::new(
-        (-1000, -1000).to(5000, 5000),
-        space_and_clip2,
-    );
-    info.hit_info = Some((0, 4));
-    builder.push_rect(&info, ColorF::new(0.5, 0.5, 0.5, 1.0));
-
-    // add a teal square to visualize the scrolling/clipping behaviour
-    // as you scroll the nested scrollbox
-    let mut info = CommonItemProperties::new((0, 200).to(50, 250), space_and_clip2);
-    info.hit_info = Some((0, 5));
-    builder.push_rect(&info, ColorF::new(0.0, 1.0, 1.0, 1.0));
-
-    // Add a sticky frame. It will "stick" twice while scrolling, once
-    // at a margin of 10px from the bottom, for 40 pixels of scrolling,
-    // and once at a margin of 10px from the top, for 60 pixels of
-    // scrolling.
-    let sticky_id = builder.define_sticky_frame(
-        space_and_clip2.spatial_id,
-        (50, 350).by(50, 50),
-        SideOffsets2D::new(Some(10.0), None, Some(10.0), None),
-        StickyOffsetBounds::new(-40.0, 60.0),
-        StickyOffsetBounds::new(0.0, 0.0),
-        LayoutVector2D::new(0.0, 0.0),
-    );
-
-    let mut info = CommonItemProperties::new(
-        (50, 350).by(50, 50),
-        SpaceAndClipInfo {
-            spatial_id: sticky_id,
-            clip_id: space_and_clip2.clip_id,
-        },
-    );
-    info.hit_info = Some((0, 6));
-    builder.push_rect(
-        &info,
-        ColorF::new(0.5, 0.5, 1.0, 1.0),
-    );
-
-    // just for good measure add another teal square further down and to
-    // the right, which can be scrolled into view by the user
-    let mut info = CommonItemProperties::new(
-        (250, 350).to(300, 400),
-        space_and_clip2,
-    );
-    info.hit_info = Some((0, 7));
-    builder.push_rect(&info, ColorF::new(0.0, 1.0, 1.0, 1.0));
 
     builder.pop_stacking_context();
     builder.pop_stacking_context();

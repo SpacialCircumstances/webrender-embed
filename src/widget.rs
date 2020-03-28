c998a301b726a3865b75b973776b4f14ef19482d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-03-28 22:16:39 +0100

    Layout for layouted text

--- src/widget.rs
+++ src/widget.rs
@@ -55,10 +55,43 @@ impl Widget for Group {
 
 struct LayoutedText<'a> {
     text: &'a str,
-    indices: Vec<Option<u32>>,
-    dimensions: Vec<Option<GlyphDimensions>>,
+    indices: Vec<u32>,
+    dimensions: Vec<GlyphDimensions>,
     font_key: FontKey,
-    inst_key: FontInstanceKey
+    inst_key: FontInstanceKey,
+    size: LayoutSize
+}
+
+impl<'a> LayoutedText<'a> {
+    fn new(text: &'a str, font_key: FontKey, inst_key: FontInstanceKey, api: &RenderApi) -> Self {
+        let indices: Vec<u32> = api
+            .get_glyph_indices(font_key, text)
+            .iter()
+            .filter_map(|&x| x)
+            .collect();
+        let dimensions: Vec<GlyphDimensions> = api
+            .get_glyph_dimensions(inst_key, indices.clone())
+            .iter()
+            .filter_map(|&x| x)
+            .collect();
+
+        let (size_x, size_y) = dimensions.iter().fold((0.0, 0.0), |(x, y), &g| {
+            let dx = (g.left + g.width) as f32 + g.advance;
+            let dy = (g.top + g.height) as f32;
+            (x, y)
+        });
+
+        let size = LayoutSize::new(size_x as f32, size_y as f32);
+
+        LayoutedText {
+            text,
+            indices,
+            dimensions,
+            font_key,
+            inst_key,
+            size
+        }
+    }
 }
 
 struct Label<'a> {
@@ -77,6 +110,6 @@ impl<'a> Label<'a> {
 
 impl<'a> Widget for Label<'a> {
     fn draw(&mut self, builder: &mut DisplayListBuilder, space_clip: SpaceAndClipInfo) -> () {
-        //TODO: Draw
+
     }
 }

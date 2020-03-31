efba31f88c458925e766f4313f553b730eb58fcd
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-03-31 22:19:40 +0200

    Add mutable store

--- src/state.rs
+++ src/state.rs
@@ -30,3 +30,27 @@ impl<T, Msg> Store<T, Msg> for ImmutableStore<T, Msg> {
         Box::new(move || sel(&self.state))
     }
 }
+
+pub struct MutableStore<T, Msg> {
+    state: T,
+    reducer: fn(&mut T, Msg)
+}
+
+impl<T, Msg> MutableStore<T, Msg> {
+    pub fn new(initial: T, reducer: fn(&mut T, Msg)) -> Self {
+        MutableStore {
+            state: initial,
+            reducer
+        }
+    }
+}
+
+impl<T, Msg> Store<T, Msg> for MutableStore<T, Msg> {
+    fn update(&mut self, msg: Msg) {
+        (self.reducer)(&mut self.state, msg);
+    }
+
+    fn selector<'a, F, O>(&'a self, sel: F) -> Box<dyn Fn () -> O + 'a> where F: Fn(&T) -> O, F: 'a {
+        Box::new(move || sel(&self.state))
+    }
+}

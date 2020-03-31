6dc7002120985cf75f48a7025823fd79e230d936
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-03-31 22:07:46 +0200

    Introduce trait for a store to allow usage of both mutable and immutable stores

--- src/state.rs
+++ src/state.rs
@@ -1,25 +1,32 @@
-pub struct Store<T, Msg> {
+pub trait Store<T, Msg> {
+    fn update(&mut self, msg: Msg);
+    fn selector<'a, F, O>(&'a self, sel: F) -> Box<dyn Fn () -> O + 'a> where F: Fn(&T) -> O, F: 'a;
+}
+
+pub struct ImmutableStore<T, Msg> {
     state: T,
     reducer: fn(&T, Msg) -> T
 }
 
-impl<T, Msg> Store<T, Msg> {
+impl<T, Msg> ImmutableStore<T, Msg> {
     pub fn new(initial: T, reducer: fn(&T, Msg) -> T) -> Self {
-        Store {
+        ImmutableStore {
             state: initial,
             reducer
         }
     }
 
-    pub fn update(&mut self, msg: Msg) {
-        self.state = (self.reducer)(&self.state, msg)
-    }
-
     pub fn set(&mut self, value: T) {
         self.state = value;
     }
+}
 
-    pub fn selector<'a, F, O>(&'a self, sel: F) -> Box<dyn Fn () -> O + 'a> where F: Fn(&T) -> O, F: 'a {
+impl<T, Msg> Store<T, Msg> for ImmutableStore<T, Msg> {
+    fn update(&mut self, msg: Msg) {
+        self.state = (self.reducer)(&self.state, msg)
+    }
+
+    fn selector<'a, F, O>(&'a self, sel: F) -> Box<dyn Fn () -> O + 'a> where F: Fn(&T) -> O, F: 'a {
         Box::new(move || sel(&self.state))
     }
 }

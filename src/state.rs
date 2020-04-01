1c69603b76f1984e905cb383b0718d3ad18d25a9
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-04-01 23:35:45 +0200

    Naive refcell based reimplementation of stores

--- src/state.rs
+++ src/state.rs
@@ -1,58 +1,66 @@
+use std::cell::{Cell, RefCell};
+use std::borrow::Borrow;
+use std::ops::Deref;
+
 pub type Selector<'a, O> = Box<dyn Fn () -> O + 'a>;
 
 pub trait Store<T, Msg> {
-    fn update(&mut self, msg: Msg);
+    fn update(&self, msg: Msg);
     fn selector<'a, F, O>(&'a self, sel: F) -> Selector<'a, O> where F: Fn(&T) -> O, F: 'a;
 }
 
 pub struct ImmutableStore<T, Msg> {
-    state: T,
+    state: RefCell<T>,
     reducer: fn(&T, Msg) -> T
 }
 
 impl<T, Msg> ImmutableStore<T, Msg> {
     pub fn new(initial: T, reducer: fn(&T, Msg) -> T) -> Self {
         ImmutableStore {
-            state: initial,
+            state: RefCell::new(initial),
             reducer
         }
     }
 
-    pub fn set(&mut self, value: T) {
-        self.state = value;
+    pub fn set(&self, value: T) {
+        let mut state = self.state.borrow_mut();
+        *state = value;
     }
 }
 
 impl<T, Msg> Store<T, Msg> for ImmutableStore<T, Msg> {
-    fn update(&mut self, msg: Msg) {
-        self.state = (self.reducer)(&self.state, msg)
+    fn update(&self, msg: Msg) {
+        let old_state = self.state.borrow();
+        let new_state = (self.reducer)(&*old_state, msg);
+        let mut state = self.state.borrow_mut();
+        *state = new_state;
     }
 
     fn selector<'a, F, O>(&'a self, sel: F) -> Selector<'a, O> where F: Fn(&T) -> O, F: 'a {
-        Box::new(move || sel(&self.state))
+        Box::new(move || sel(&*self.state.borrow()))
     }
 }
 
 pub struct MutableStore<T, Msg> {
-    state: T,
+    state: RefCell<T>,
     reducer: fn(&mut T, Msg)
 }
 
 impl<T, Msg> MutableStore<T, Msg> {
     pub fn new(initial: T, reducer: fn(&mut T, Msg)) -> Self {
         MutableStore {
-            state: initial,
+            state: RefCell::new(initial),
             reducer
         }
     }
 }
 
 impl<T, Msg> Store<T, Msg> for MutableStore<T, Msg> {
-    fn update(&mut self, msg: Msg) {
-        (self.reducer)(&mut self.state, msg);
+    fn update(&self, msg: Msg) {
+        (self.reducer)(&mut *self.state.borrow_mut(), msg);
     }
 
     fn selector<'a, F, O>(&'a self, sel: F) -> Selector<'a, O> where F: Fn(&T) -> O, F: 'a {
-        Box::new(move || sel(&self.state))
+        Box::new(move || sel(&*self.state.borrow()))
     }
 }
